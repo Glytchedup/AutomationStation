c19f3ce7020e8a6295115ac5e0ff3bb5b2f4aa83
ilyazarudny <zaruda46@gmail.com>
2018-10-05 13:08:08 +0300

    slack chat component was integrated

--- client/src/components/Slack/Slack.jsx
+++ client/src/components/Slack/Slack.jsx
@@ -1,46 +1,24 @@
 import React, {Component} from 'react';
-// import { ReactSlackChat } from 'react-slack-chat';
+import { ReactSlackChat } from 'react-slack-chat';
+
+import botImage from './images/bot.png';
 
 class SlackComponent extends Component {
   render() {
-    return (
-        
-      <div>
-      <img src="https://a.slack-edge.com/3aabd6/marketing/img/pricing/enterprise_grid_logo.png" size={50} alt="placeholder"/>
-      <p>Slack Integration</p>
-      </div>
-
-        
-//         <ReactSlackChat
-//           botName='5punk-unique-id' // VisitorID, CorpID, Email, IP address etc.
-//           apiToken='xoxb-105098869779-441001243173-pABCuDm8nT50SKnXqKDnm9E9'
-//           channels={[
-//           {
-//             name: 'random'
-//           },
-//           {
-//             name: 'test',
-//             id: 'G3Z3247C2',
-//             icon: ''
-//           },
-//           {
-//             name: 'test22',
-//             id: '',
-//             icon: './logo.svg'
-//           }]}
-//           helpText='Optional Help Text'
-//           themeColor='#856090'
-//           userImage='http://www.iconshock.com/img_vista/FLAT/mail/jpg/robot_icon.jpg'
-//           debugMode={true}
-//           hooks={[
-//             {
-//               /* My Custom Hook */
-//               id: 'getSystemInfo',
-//               action: () => 'MY SYSTEM INFO!'
-//             }
-//           ]}
-//         />
-      )
+    return (        
+      <ReactSlackChat
+        botName="testbot1"
+        apiToken="eG94Yi0zNDIzMTMyMTE0MDgtNDQ5OTQwMDg2OTk0LWVPcTIxdHNUZEZTZGxiMDlXd0pIUHdlRw=="
+        channels={[
+          {
+            name: 'project-3-test'
+          }
+        ]}
+        helpText='Need Help?'
+        themeColor='#007bff'
+        userImage={botImage}
+      />
+    )
   }
 }
 
