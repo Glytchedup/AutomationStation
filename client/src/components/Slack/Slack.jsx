771af2a963846deafcb11db519e35d41bd66dc13
David <davekubo@gmail.com>
2018-10-05 13:17:17 -0700

    working slack

--- client/src/components/Slack/Slack.jsx
+++ client/src/components/Slack/Slack.jsx
@@ -1,20 +1,31 @@
 import React, {Component} from 'react';
 import { ReactSlackChat } from 'react-slack-chat';
 
-import botImage from './images/bot.png';
+// import botImage from './images/bot.png';
+import botImage from './images/square.png';
 
 class SlackComponent extends Component {
   render() {
     return (        
       <ReactSlackChat
+      //Test Slack Room
         botName="testbot1"
         apiToken="eG94Yi0zNDIzMTMyMTE0MDgtNDQ5OTQwMDg2OTk0LWVPcTIxdHNUZEZTZGxiMDlXd0pIUHdlRw=="
+        
+        //Work Slack Room
+        // botName="rmasbot"
+        // apiToken="eG94Yi0xMDUwOTg4Njk3NzktNDUwOTg2NDE0NTE5LUZPd0ZxdEJzdXZJdFRWZnZybFRUQmZnYg=="
+
         channels={[
-          {
-            name: 'project-3-test'
-          }
+          {name: 'p3_slack'},
+          {name: 'p3t2',
+          id: 'GD8A395K4',
+        }
+            //Work room
+            // name: 'bayteamvmo'
+            // name: 'test-temp'
         ]}
-        helpText='Need Help?'
+        helpText='Slack 🌎'
         themeColor='#007bff'
         userImage={botImage}
       />
