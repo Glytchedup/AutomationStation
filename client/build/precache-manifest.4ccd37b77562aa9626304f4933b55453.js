5a5317e5951d58520c65233296357ec79da2b747
David Kuboshige <davekubo@gmail.com>
2018-10-09 21:33:01 -0700

    herplu

---
+++ client/build/precache-manifest.4ccd37b77562aa9626304f4933b55453.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc92835d16ffefa0733f5b62886626c2",
+    "url": "/static/media/square.fc92835d.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "496113e296cffaab9565",
+    "url": "/static/js/main.496113e2.chunk.js"
+  },
+  {
+    "revision": "a5d463f881a7cf5e9d38",
+    "url": "/static/js/1.a5d463f8.chunk.js"
+  },
+  {
+    "revision": "496113e296cffaab9565",
+    "url": "/static/css/main.3e0671eb.chunk.css"
+  },
+  {
+    "revision": "e233c1ea75a49fa2b8d22918fc22e6b7",
+    "url": "/index.html"
+  }
+];
