c14d941864e6877dda5bd3b37b6e0743f30bb5e7
David Kuboshige <davekubo@gmail.com>
2018-10-23 22:37:16 -0700

    added flipcard

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import SlackComponent from "./components/Slack/Slack";
 import PortfolioComponent from "./components/Portfolio/Portfolio";
+import FlipcardComponent from "./components/Portfolio/components/FlipCard/flipcard"
+// var auth = require('./routes/auth');
+
+
 // import Dashboard from "./components/Portfolio/components/pages/dashboard/Dashboard"
 // import FormsPage from "./components/signin/FormsPage"
 
@@ -12,12 +16,14 @@ const App = () => (
         </header>
         <p className="App-intro" />
         <div className="App">
-      
           <PortfolioComponent />
         </div>
         <div className="App">
-          <SlackComponent />
+          {/* <SlackComponent /> */}
         </div>
+        {/* <div className="App">
+          <FlipcardComponent />
+        </div> */}
           {/* <Dashboard /> */}
           {/* <FormsPage /> */}
       </div>
--- client/src/components/Portfolio/components/pages/Detail/Detail.js
+++ client/src/components/Portfolio/components/pages/Detail/Detail.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../Grid";
 import Jumbotron from "../../Jumbotron";
 import API from "../../../../../utils/API";
+import './detail.css';
+
 
 class Detail extends Component {
   state = {
@@ -10,45 +12,74 @@ class Detail extends Component {
   };
   componentDidMount() {
     API.getHotel(this.props.match.params.marsha)
-      .then(res => this.setState({ hotel: res.data}))
-      .catch(err => console.log(err));
+    .then(res => this.setState({ hotel: res.data}))
+    .catch(err => console.log(err));
   }
-
+  
   render() {
     const keys = ["synopsis","IO","title","address","city","state","zip","phone","date"];
     return (
-      <Container fluid>
-        <Row>
-          <Col size="md-12">
-            <Jumbotron>
-              <h1>{this.state.hotel.marsha}</h1>
-            </Jumbotron>
-          </Col>
-        </Row>
-        <div className="card text-gray bg-dark info">
-        <Row>
+       <Container fluid>
+      <div className="card2">
+      <div className="card__content" >
+                  
+      <div className="card__front" src = {this.state.hotel.photo} alt = "hotel" >
+      {/* <img src = {this.state.hotel.photo} alt = "hotel"/> */}
+      <h3 className="card__title">
+      <h1>{this.state.hotel.marsha}</h1>
+      </h3>
+      <p className="card__subtitle">
+      <h1>{this.state.hotel.city}</h1>
+      </p>
+      </div>       
+      <div className="card__back card__body card text-dark bg-dark info">
+      {/* <p className="card__body card text-grey bg-dark info"> */}
             {keys.map(key => (
-                <Col size="md-5 md-offset-1" key = {key}>
-                <article>
-                  <h1>{key}</h1>
+                <article key = {key}>
+                  {/* <h1>{key}</h1> */}
                   <p>{this.state.hotel[key]}</p>
                 </article>
-              </Col>
             ))}
-        </Row>
-        </div>
-        <Row>
-        <div className="card text-gray bg-light md-5">
-          <Col size="md-5 md-offset-1">
-          <img src = {this.state.hotel.photo} alt = "hotel"/>
-          </Col>
-        </div>
-        </Row>
-        <Row>
-          <Col size="md-2">
+            {/* </p> */}
+
+      </div>
+      </div>
+      </div>     
+     
+    {/* //     <Row> */}
+    {/* //       <Col size="md-12"> */}
+    {/* //         <Jumbotron> */}
+    {/* //         </Jumbotron> */}
+    {/* //       </Col> */}
+    {/* //     </Row> */}
+   
+
+    {/* // <div className="card text-gray bg-dark info"> */}
+    {/* //     <Row> */}
+    {/* //         {keys.map(key => ( */}
+    {/* //             <Col size="md-5 md-offset-1" key = {key}> */}
+    {/* //             <article> */}
+    {/* //               <h1>{key}</h1> */}
+    {/* //               <p>{this.state.hotel[key]}</p> */}
+    {/* //             </article> */}
+    {/* //           </Col> */}
+    {/* //         ))} */}
+    {/* //     </Row> */}
+    {/* //     </div> */}
+   
+   
+        {/* // <Row>
+        // <div className="card text-gray bg-light md-5">
+    //       <Col size="md-5 md-offset-1">
+          // <img src = {this.state.hotel.photo} alt = "hotel"/>
+    //       </Col>
+    //     </div>
+    //     </Row>
+    //     <Row>
+    //       <Col size="md-2">
             <Link to="/">← Back to Main Page</Link>
-          </Col>
-        </Row>
+    //       </Col>
+    //     </Row> */}
       </Container>
     );
   }
